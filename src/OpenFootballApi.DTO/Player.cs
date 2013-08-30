b427824d466ca6bed77926cae63f6f280a8906c7
Joe Kampschmidt <jokecamp@gmail.com>
2013-08-30 15:12:17 -0400

    First Commit
    Baseline project. No third party dependencies yet.

---
+++ src/OpenFootballApi.DTO/Player.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OpenFootballApi.DTO
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ src/OpenFootballApi.DTO/Tag.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OpenFootballApi.DTO
+{
+    /// <summary>
+    /// A descriptive tag/attribute that will describe mainly players
+    /// </summary>
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
